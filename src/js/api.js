22e29b6b5c307e4d752a13bd68587190a18f3ecb
ania.liulka <anialiulka@gmail.com>
2023-06-17 10:44:43 -0400

    Added simplelightbox

--- src/js/api.js
+++ src/js/api.js
@@ -1,25 +1,29 @@
-const API_KEY = '37360238-906099131e02ce03408024f3e';
+// const API_KEY = '37360238-906099131e02ce03408024f3e';
 
-export async function fetchImages(searchQuery) {
-  try {
-    const searchParams = new URLSearchParams({
-      key: API_KEY,
-      q: `${searchQuery}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: 40,
-      page: 1,
-    });
-    const url = `https://pixabay.com/api/?${searchParams}`;
-    console.log(searchParams.toString());
-    return fetch(url).then(response => {
-      console.log(response.json);
-      return response.json();
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
+// let per_page = 40;
+// export let page = 1;
 
-export async function fetchMoreImages() {}
+// export async function fetchImages(searchQuery) {
+//   try {
+//     const searchParams = new URLSearchParams({
+//       key: API_KEY,
+//       q: `${searchQuery}`,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: true,
+//       per_page,
+//       page,
+//     });
+//     const url = `https://pixabay.com/api/?${searchParams}`;
+//     console.log(searchParams.toString());
+
+//     return fetch(url).then(response => {
+//       console.log(response.json);
+//       return response.json();
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// export async function fetchMoreImages() {}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
-import { fetchImages, fetchMoreImages } from './api';
+// import { fetchImages, fetchMoreImages, page } from './api';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_KEY = '37360238-906099131e02ce03408024f3e';
+
+let per_page = 40;
+let page = 1;
+let test = null;
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
@@ -9,21 +17,50 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
+  page = 1;
+
   const {
     elements: { searchQuery },
   } = event.currentTarget;
-  console.log(searchQuery.value);
-  fetchImages(searchQuery.value).then(({ total, hits }) => {
-    loader.classList.remove('hidden');
+
+  test = searchQuery.value;
+
+  fetchImages(test).then(({ total, hits }) => {
     console.log(hits);
+
     if (hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     } else renderGallery(hits);
+    page += 1;
+    if (page > 1) {
+      loader.classList.remove('hidden');
+    }
     showTotal(total);
   });
+}
+async function fetchImages(searchQuery) {
+  try {
+    const searchParams = new URLSearchParams({
+      key: API_KEY,
+      q: `${searchQuery}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page,
+      page,
+    });
+    const url = `https://pixabay.com/api/?${searchParams}`;
+
+    return fetch(url).then(response => {
+      console.log(response.json);
+      return response.json();
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function renderGallery(pictures) {
@@ -38,7 +75,7 @@ function renderGallery(pictures) {
         comments,
         downloads,
       }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="main-image" />
+<a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" class="main-image" /></a>
   <div class="info">
     <p class="info-item">
       <b> <span class="material-symbols-outlined">
@@ -64,16 +101,26 @@ download
 </div>`
     )
     .join('');
-
-  gallery.innerHTML = markup;
+  if (page === 1) {
+    gallery.innerHTML = markup;
+  } else gallery.innerHTML += markup;
+  new SimpleLightbox('.photo-card a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
 }
 
 function showTotal(total) {
   Notiflix.Notify.success(`Hooray! We found ${total} images.`);
 }
 
-loader.addEventListener('click', onLoaderClick);
+loader.addEventListener('click', loadMore);
 
-function onLoaderClick(event) {
-  fetchMoreImages();
+function loadMore(event) {
+  event.preventDefault();
+  fetchImages(test).then(({ hits }) => {
+    console.log(hits);
+    renderGallery(hits);
+    page += 1;
+  });
 }
